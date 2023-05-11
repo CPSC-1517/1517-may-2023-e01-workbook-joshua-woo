7208e6c395c26db0e522177a250e59887e4a86ca
joshua-woo <joshua-woo@hotmail.com>
2023-05-10 18:41:27 -0600

    Created loop

--- Github Practice Solution/Github-Test/Github-Test/Program.cs
+++ Github Practice Solution/Github-Test/Github-Test/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Josh, this is the wonderful world of github");
-for(int i=0; i<10, i++)
+for(int i=0; i<10; i++)
 {
     Console.WriteLine($"iteration number {i}");
 }
