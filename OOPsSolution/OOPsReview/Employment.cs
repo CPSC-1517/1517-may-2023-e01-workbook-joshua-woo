f50363862f977b62c1476e03054c9019be641091
joshua-woo <joshua-woo@hotmail.com>
2023-05-15 18:54:27 -0600

    May 15

--- OOPsSolution/OOPsReview/Employment.cs
+++ OOPsSolution/OOPsReview/Employment.cs
@@ -48,6 +48,7 @@ namespace OOPsReview
           //referred to as the mutator
           //it is within the set that the validation of the data
           //is done to determine if the data is acceptable
+          // by default sets are a default
           set 
             {
                 if (string.IsNullOrWhiteSpace(value))
@@ -56,6 +57,121 @@ namespace OOPsReview
                 }
                 _Title = value;
             } 
+        }
+        public SupervisoryLevel Level
+        { 
+
+        get { return _Level; }
+
+            set
+            {
+                /*
+                 * a private set means that the property can only be set by code within the
+                 * class definition
+                 * an advantage of doing this is increasing security on the field
+                 * as any change is under the control of the class definition
+                 * 
+                 * validate the value given as an enum is actually defined
+                 * 
+                 * A user of this class could send in an integer value that was type
+                 * cast as this enum datatype BUT have a non defined value
+                 * 
+                 * to test for a defined enum value use Enum.IsDefined(typeof(xxxx), value)
+                 * where xxxx is the name of the enum datatype
+                 */
+                if (!Enum.IsDefined(typeof(SupervisoryLevel), value))
+                {
+                throw new ArgumentException($"Supervisory level is invalid: {value}");
+                }
+                _Level = value;
+            } 
+        }
+        public double Years
+        {
+            get { return _Years; } // used on the right side of an assignment statement or in an expression
+            set
+            {
+                //if (value < 0) //using a Utilities generic method to do this test
+                if (!Utilities.IsZeroOrPositive(value))
+                {
+                    throw new ArgumentOutOfRangeException("value");
+                }
+                _Years = value;
+            }
+            /*
+             * this property is an example of an auto-implemented property
+             * there is no validation within the property
+             * there is no private data member for this property
+             * the system will generate an internal storage area for the data
+             * and handle the setting and getting from that storage area 
+             * the private set means that the property will only be able to be set via constructor or method
+             * auto-implemented properties can have either a public or private set
+             * using a public or private set is a design decision
+             */
+        }
+        public DateTime StartDate { get; private set; }
+
+        /*
+         * Constructors
+         * 
+         * the purpose of a constructor is to create an instance of your class in a known state
+         * 
+         * does a class definition need a constructor? No
+         * If a class definition does not have a constructor then the system will
+         * create the instance and assign the system default value to the data member
+         * and/or auto implemented property
+         * if you have no constructor the common phrase is "using a system constructor"
+         * 
+         * 
+         * Default Constructor
+         * you can apply your own literal values for your data members and/or auto-implemented properties
+         * that differ from the system default values
+         * why? you may have data that is validated and using the system default values would cause an exception
+         * 
+         * I you code a constructor in your class you are responsible for all constructors in your class
+         */
+        public Employment()
+        {
+            //this constructor will be used o""n creating an instance using
+            //Employment myInstance=new Employment ();
+            //A practice that I personally use is to avoid reffering my data members directly
+            //specifcally if the property contains validation
+            Title = "Unknown";
+            Level = SupervisoryLevel.TeamMember;
+            StartDate = DateTime.Now;  
+            /*optionally one could set years to zero but that is the system default
+             value for a numeric, therefore one does not need to assign a value
+            unless you wish to*/
+        }
+
+        //Greedy Constructor
+        /*
+         * a greedy constructor is on that accepts a parameter list of values to 
+         * assign to your instance data on creation of the instance
+         * generally your greedy constructor contains a perameter on the signature
+         * for each data member you have in your class definition
+         */
+
+        /*all optional perameters must appear after non-optional parameters in your parameters list
+        in this example we ill use years as an optional parameter
+         */
+        public Employment(string title, SupervisoryLevel level, DateTime startdate, double years =0.0)
+        {
+            Title = title;
+            Level = level;
+            Years = years;
+            /*
+             *this example to demonstrate where your can place validation for
+             *properties have are auto-implemented
+             *validation start date must not exist in the future
+             *validation can be done anywhere in your class
+             *since the property is auto-implemented and has a private set
+             *validation can be done in the constructor or a method
+             *that alters the property. It would not be an
+             *auto-implemented property but a fully implemented property
+             *Today has a time of 00.00:000 AM
+             *.Now has a specific time of day execution 18
+             */
         }
     }
 }
---
+++ OOPsSolution/OOPsReview/Utilities.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public static class Utilities
+    {
+        /*
+         * static classes are not instantiated by the outside user (developer/code)
+         * static class items are referenced using: classname.xxxx
+         * methods within this class have the keyword static in their signature
+         * static classes are shared between all outside users at the same time
+         * Do not consider saving data within a static class because you cannot be certain
+         * it will be there when you use the class again
+         * consider placing generic re-usable methods with a static class
+         * 
+         * sample of a generic method: numeric is a zero or a positive value
+         */
+        public static bool IsZeroOrPositive(double value)
+        {
+            /*
+             * a structure method (apply to loops, ect) will have a single entry point
+             * and a single exit point
+             * in this course you will avoid where possible multiple returns from a method
+             * in this course you will avoid using a break to exit a loop structure or if structure
+             */
+            bool valid = true;
+            if (value < 0) 
+            {
+                valid = false;
+            }
+            return valid;
+        }
+        public static bool IsZeroOrPositive(int value)
+        {
+            /*
+             * a structure method (apply to loops, ect) will have a single entry point
+             * and a single exit point
+             * in this course you will avoid where possible multiple returns from a method
+             * in this course you will avoid using a break to exit a loop structure or if structure
+             */
+            bool valid = true;
+            if (value < 0)
+            {
+                valid = false;
+            }
+            return valid;
+        }
+        public static bool IsZeroOrPositive(decimal value)
+        {
+            /*
+             * a structure method (apply to loops, ect) will have a single entry point
+             * and a single exit point
+             * in this course you will avoid where possible multiple returns from a method
+             * in this course you will avoid using a break to exit a loop structure or if structure
+             */
+            bool valid = true;
+            if (value < 0.0m)
+            {
+                valid = false;
+            }
+            return valid;
+        }
+    }
+}
